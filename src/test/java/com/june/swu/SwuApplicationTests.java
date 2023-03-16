2f4930715176bcaf61a37bf1a52362f2069c21b2
hi-june <wnsgur735@gmail.com>
2023-03-16 16:05:42 +0900

    initial commit

---
+++ src/test/java/com/june/swu/SwuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.june.swu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
