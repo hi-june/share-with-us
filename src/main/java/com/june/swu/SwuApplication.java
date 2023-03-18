f52504803485666052e40c0a6e6697fa1aa2b847
hi-june <wnsgur735@gmail.com>
2023-03-19 03:03:18 +0900

    feat: BaseEntity 적용

--- src/main/java/com/june/swu/SwuApplication.java
+++ src/main/java/com/june/swu/SwuApplication.java
@@ -2,7 +2,9 @@ package com.june.swu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class SwuApplication {
 
